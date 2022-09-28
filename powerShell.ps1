ddc0f6ba55f63fe1ffa04290a79eaadeca511bd3
Senichkin_al <aleksandr_senichkin@senla.eu>
2022-09-28 12:13:25 +0300

    install some shit

--- powerShell.ps1
+++ powerShell.ps1
@@ -8,3 +8,20 @@ Get-appxpackage -allusers *WindowsFeedbackHub* | Remove-AppxPackage
 Get-appxpackage -allusers *xbox* | Remove-AppxPackage
 Get-appxpackage -allusers *store* | Remove-AppxPackage
 Get-appxpackage -allusers *groove* | Remove-AppxPackage
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install firefox
+choco install notepadplusplus
+choco install 7zip
+choco install coretemp
+choco install telegram
+
+choco install docker-desktop
+choco install postman
+choco install sourcetree
+choco install dbeaver
+choco install jetbrains-rider
+
+choco install windirstat
+
