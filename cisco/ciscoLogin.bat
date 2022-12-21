1f155a55a28cb03a46b2fc52fabea83bf72f5531
SashaSenichkin <30441534+SashaSenichkin@users.noreply.github.com>
2022-12-21 22:22:27 +0300

    Update ciscoLogin.bat

--- cisco/ciscoLogin.bat
+++ cisco/ciscoLogin.bat
@@ -1,4 +1,4 @@
 node generator.mjs>>.login_info
 "C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpncli.exe" -s < ".login_info"
-Powershell.exe -executionpolicy remotesigned -File clear7.ps1
+Powershell.exe -executionpolicy Bypass -File clear7.ps1
 pause
