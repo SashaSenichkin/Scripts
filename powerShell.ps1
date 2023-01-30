48b1039552f78df6849d33f43cd14bdfe9d8a606
SashaSenichkin <30441534+SashaSenichkin@users.noreply.github.com>
2023-01-30 16:08:41 +0300

    Update powerShell.ps1

--- powerShell.ps1
+++ powerShell.ps1
@@ -31,6 +31,8 @@ choco install dbeaver -y
 choco install jetbrains-rider -y
 choco install nodejs -y
 choco install obs-studio -y
+
+choco install putty  -y
 choco install mtputty -y
 
 choco install windirstat -y
